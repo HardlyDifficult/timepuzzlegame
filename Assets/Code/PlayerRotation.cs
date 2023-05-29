2561c835449dcd66407ed62c287684368bed495e
HardlyDifficult <hardlydiff@gmail.com>
2023-05-29 11:41:00 -0700

    Look up!

--- Assets/Code/PlayerRotation.cs
+++ Assets/Code/PlayerRotation.cs
@@ -5,16 +5,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerRotation : MonoBehaviour
 {
+    Vector3 targetRotation;
     float inputRotation;
+    float inputVerticle;
+
+    [SerializeField]
+    float clampY = 10;
+
+    [SerializeField]
+    float ySpeed = .1f;
+
+    void Start() {
+        targetRotation = transform.rotation.eulerAngles;
+    }
 
     void OnLook(InputValue value)
     {
         inputRotation += value.Get<Vector2>().x;
+        inputVerticle += value.Get<Vector2>().y;
     }
 
     private void FixedUpdate()
     {
-        transform.rotation *= Quaternion.Euler(0, inputRotation, 0);
+        targetRotation += new Vector3(inputVerticle * ySpeed, inputRotation, 0);
+        if(Mathf.Abs(targetRotation.x) > clampY) {
+            targetRotation.x = Mathf.Sign(targetRotation.x) * clampY;
+        } 
+        transform.rotation = Quaternion.Euler(targetRotation);
         inputRotation = 0;
+        inputVerticle = 0;
     }
 }
